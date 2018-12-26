ed8aec11c73847ae7bda7d09ec8e92959037f0b9
Paulo Henrique Gomes <paulo.gomes@dacasafin.onmicrosoft.com>
2018-12-26 12:45:25 -0200

    Minimum necessary to generate the PDF

--- HtmlToPdf/Program.cs
+++ HtmlToPdf/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using DinkToPdf;
 
 namespace HtmlToPdf
 {
@@ -6,7 +6,37 @@ namespace HtmlToPdf
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // A4 em pixels ==> 595 x 842 (altura x largura)
+
+            string path = @"C:\Users\paulo.gomes\Desktop\HtmlToPdf\TemplateFatura\{0}";
+            var html = @"Template_Fatura.html";
+            var css = @"styles.css";
+            //var faturaId = 12345678;
+
+            string pdfText = System.IO.File.ReadAllText(string.Format(path, html));
+
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4Plus,
+                },
+                Objects = {
+                    new ObjectSettings() {
+                        PagesCount = true,
+                        HtmlContent = pdfText,
+                        WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = string.Format(path, css) }
+                    }
+                }
+            };
+
+            var pdf = new BasicConverter(new PdfTools()).Convert(doc);
+
+            using (System.IO.FileStream stream = new System.IO.FileStream(string.Format(path, $"output.pdf"), System.IO.FileMode.Create))
+            {
+                stream.Write(pdf, 0, pdf.Length);
+            }
         }
     }
 }
